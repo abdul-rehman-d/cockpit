270fbced55a81a4b98a613c98222c73835581c35
Abdul Rehman <ar618211@gmail.com>
2026-03-16 19:34:00 +0500

    feat: add usage service and progress bar to model

--- internal/view/model.go
+++ internal/view/model.go
@@ -1,38 +1,68 @@
 package view
 
 import (
-	"fmt"
 	"strings"
+	"time"
 
 	"charm.land/bubbles/v2/help"
 	"charm.land/bubbles/v2/key"
+	"charm.land/bubbles/v2/progress"
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
+	"github.com/abdul-rehman-d/cockpit/internal/usage"
+)
+
+type tickMsg time.Time
+
+var (
+	yellow = lipgloss.Color("#FDFF8C")
+	pink   = lipgloss.Color("#FF7CCB")
 )
 
 type Model struct {
-	keys       keyMap
-	help       help.Model
-	inputStyle lipgloss.Style
-	lastKey    string
-	quitting   bool
-	height     int
-	width      int
-	ready      bool
+	// help menu thingy
+	keys keyMap
+	help help.Model
+
+	// dimensions of the terminal
+	width  int
+	height int
+
+	// state
+	quitting bool
+	ready    bool
+
+	// reusable progress bar
+	prog progress.Model
+
+	// usage service
+	service *usage.Service
+
+	// misc ui stuff
+	maxSampleNameLength int
 }
 
 func NewModel() Model {
+	service := usage.NewService()
+	samples := service.GetAllSamples()
+	maxSampleNameLength := 0
+	for _, sample := range samples {
+		maxSampleNameLength = max(maxSampleNameLength, len(sample.Name))
+	}
 	return Model{
-		keys:       keys,
-		help:       help.New(),
-		inputStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("#FF75B7")),
-		height:     10,
-		ready:      false,
+		keys: keys,
+		help: help.New(),
+
+		prog: progress.New(progress.WithScaled(true), progress.WithColors(pink, yellow)),
+
+		service: service,
+
+		maxSampleNameLength: maxSampleNameLength,
 	}
 }
 
 func (m Model) Init() tea.Cmd {
-	return nil
+	return doTick()
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -44,6 +74,10 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		// If we set a width on the help menu it can gracefully truncate
 		// its view as needed.
 		m.help.SetWidth(msg.Width)
+		m.prog.SetWidth(msg.Width - m.maxSampleNameLength - 5) // 5 = one space between name and bar and 4 spaces of padding
+
+	case tickMsg:
+		return m, doTick()
 
 	case tea.KeyPressMsg:
 		switch {
@@ -74,13 +108,32 @@ func (m Model) View() tea.View {
 	availableHeight := m.height - 3 - strings.Count(helpView, "\n")
 
 	s := ""
-	for i := range availableHeight {
-		s += "["
-		s += fmt.Sprintf("%d  %d", availableHeight, i+1)
-		s += "\n"
+
+	samples := m.service.GetAllSamples()
+	for _, sample := range samples {
+		s += m.renderSample(sample)
 	}
+
+	availableHeight -= strings.Count(s, "\n")
+	s += strings.Repeat("\n", availableHeight)
 
 	view := tea.NewView(s + "\n" + helpView)
 	view.AltScreen = true
 	return view
+}
+
+func (m Model) renderSample(sample usage.Sample) string {
+	row1 := sample.Name +
+		strings.Repeat(" ", m.maxSampleNameLength-len(sample.Name)+1) +
+		m.prog.ViewAs(sample.Value)
+	row2 := strings.Repeat(" ", m.maxSampleNameLength+1) +
+		sample.ValueInWords
+
+	return row1 + "\n" + row2 + "\n\n"
+}
+
+func doTick() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
 }
